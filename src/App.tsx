24a6196418a3074cbd369d601b5c353b50189305
Pyother <piotr.dominik.sobol@gmail.com>
2025-07-08 18:29:24 +0200

    Initial commit, project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// * React:
+import React from 'react';
+
+// * Styles:
+import './styles/globals.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to PyOther Lucchese Demo</h1>
+                <p>This is a simple React application.</p>
+            </header>
+        </div>
+    )
+}
+
+export default App;
