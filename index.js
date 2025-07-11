b65c6008a8a9084ff6b9293937d62c369a7da712
Pyother <piotr.dominik.sobol@gmail.com>
2025-07-11 11:28:30 +0200

    Separate production port from deploy port

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
+// * == Imports and constants ==
 const express = require('express'),
       cors = require('cors'),
       fs = require('fs'),
       path = require('path'),
       { Liquid } = require('liquidjs');
+const port = process.env.PORT || 50034;
+// * =============
 
-const app = express();
+
+// * == Liquid setup ==
 const engine = new Liquid({
-  root: path.resolve(__dirname, 'views'),
+  root: [
+    path.resolve(__dirname, 'views'),
+    path.resolve(__dirname, 'components'),
+    path.resolve(__dirname, 'components/layout'),
+    path.resolve(__dirname, 'components/content'),
+  ],
   extname: '.liquid'
 });
+// * =============
 
+
+// * == Express setup =
+const app = express();
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.engine('liquid', engine.express());
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'liquid');
+// * =============
 
+
+// * == Routes ==
 app.get('/', (req, res) => {
     const raw_products = fs.readFileSync(path.resolve(__dirname, 'data/sample.json'));
     const raw_texts = fs.readFileSync(path.resolve(__dirname, 'data/texts.json'));
@@ -30,5 +46,8 @@ app.get('/api/products', (req, res) => {
   const raw = fs.readFileSync(path.resolve(__dirname, 'data/sample.json'));
   res.json(JSON.parse(raw));
 });
+// * =============
 
-app.listen(50036, () => console.log('Server listening on port 50034'));
+
+// * App:
+app.listen(port, () => console.log(`App is running on http://localhost:${port}`));
