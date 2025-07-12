8fe44a819191a00a85f0f0cd288fe22d05c62e41
Pyother <piotr.dominik.sobol@gmail.com>
2025-07-12 15:17:56 +0200

    Migrate data to the external AWS API and implement fetching function

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const express = require('express'),
       fs = require('fs'),
       path = require('path'),
       { Liquid } = require('liquidjs');
+require('dotenv').config();
 const port = process.env.PORT || 50034;
+const externalAPI = process.env.EXTERNAL_API;
+let products, texts;
 // * =============
 
 
@@ -31,23 +34,35 @@ app.set('view engine', 'liquid');
 // * =============
 
 
-// * == Routes ==
-app.get('/', (req, res) => {
-    const raw_products = fs.readFileSync(path.resolve(__dirname, 'data/sample.json'));
-    const raw_texts = fs.readFileSync(path.resolve(__dirname, 'data/texts.json'));
-
-    const products = JSON.parse(raw_products).products;
-    const texts = JSON.parse(raw_texts);
-
-    res.render('main', { products, texts });
-});
-
-app.get('/api/products', (req, res) => {
-  const raw = fs.readFileSync(path.resolve(__dirname, 'data/sample.json'));
-  res.json(JSON.parse(raw));
-});
+// * == Fetching ==
+async function loadData (url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const json = await response.json();
+  return json;
+}
 // * =============
 
 
-// * App:
-app.listen(port, () => console.log(`App is running on http://localhost:${port}`));
+async function startServer() {
+  try {
+    products = await loadData(`${externalAPI}/api/products`);
+    texts = await loadData(`${externalAPI}/api/texts`);   
+
+    // * == Routes ==
+    app.get('/', (req, res) => {
+      res.render('main', { products, texts });
+    });
+    // * =============
+
+    app.listen(port, () => console.log(`App is running on http://localhost:${port}`));
+
+  } catch (err) {
+    console.error('Błąd podczas ładowania danych:', err);
+    process.exit(1); 
+  }
+}
+
+startServer();
